7eb27b5cd3d516998cbdf0724ec57493a75641ea
Abdul-Taufiq <abdul.taufiq1712@gmail.com>
2026-02-19 15:10:36 +0700

    rev7

--- resources/views/page/master-perjanjian-kredit/addendum/pasal/header-&-premis.blade.php
+++ resources/views/page/master-perjanjian-kredit/addendum/pasal/header-&-premis.blade.php
@@ -243,14 +243,14 @@
     </table>
     <b>BANK</b> dan <b>DEBITUR</b> dengan ini menerangkan terlebih dahulu sebagai berikut: <br>
     Bahwa <b>BANK</b> dan <b>DEBITUR</b> telah melakukan Perjanjian Kredit
-    @if ($pkpmk->pkpmk->jns_akta == 'Notariil')
-        dengan Akta Notaris Nomor {{ $pkpmk->pkpmk->no_akta_notaris }} Tanggal
+    @if ($pkpmk->jns_akta == 'Notariil')
+        dengan Akta Notaris Nomor {{ $pkpmk->no_akta_notaris }} Tanggal
         {{ $pkpmk->pkpmk->tgl_akta_notaris->translatedFormat('d F Y') }}
     @else
-        Nomor {{ $pkpmk->pkpmk->no_pkpmk }} tanggal
-        {{ $pkpmk->pkpmk->tgl_pkpmk->translatedFormat('d F Y') }}
+        Nomor {{ $pkpmk->no_pkpmk }} tanggal
+        {{ $pkpmk->tgl_pkpmk->translatedFormat('d F Y') }}
         yang dibuat secara
-        @if ($pkpmk->pkpmk->jns_pengikatan == 'Bawah Tangan/Legalisasi')
+        @if ($pkpmk->jns_pengikatan == 'Bawah Tangan/Legalisasi')
             dibawah tangan
         @else
             Legalisasi
