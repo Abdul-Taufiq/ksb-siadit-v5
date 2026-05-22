3d67278ddd2047d69c43a2a7cd12123b38633400
Abdul-Taufiq <abdul.taufiq1712@gmail.com>
2026-05-22 11:14:36 +0700

    z

--- app/Services/MasterKredit/Muk/MukServiceEdit.php
+++ app/Services/MasterKredit/Muk/MukServiceEdit.php
@@ -356,7 +356,7 @@ class MukServiceEdit
 
                 // update nilai agunan dan nilai taksasi di tanah
                 $jamTanah = JamTanah::find($id_tanah);
-                if (isset($data['data_1_' . $i]) && $data['data_1_' . $i] !== null && empty($data['bangunan_1_' . $i])) {
+                if (!empty($data['data_1_' . $i]) && empty($data['bangunan_1_' . $i])) {
                     $jamTanah->update([
                         'nilai_jaminan' => $this->normalizeNumber($data['nilai_agunan_' . $i]),
                         'nilai_taksasi' => $this->normalizeNumber($data['kes_nilai_taksasi_' . $i])
@@ -519,7 +519,7 @@ class MukServiceEdit
                         $rekap2 = new SC_Tanah_Rekap_2_Vanalis();
                     }
 
-                    if (isset($data['data_1_' . $i]) && $data['data_1_' . $i] !== null && empty($data['bangunan_1_' . $i])) {
+                    if (!empty($data['data_1_' . $i]) && empty($data['bangunan_1_' . $i])) {
                         $rekap1->id_muk = $id_muk;
                         $rekap1->id_jaminan_pertanahan = $id_tanah;
                         $rekap1->nilai_njop = $this->normalizeNumber($data['nilai_njop_' . $i]);
@@ -705,7 +705,7 @@ class MukServiceEdit
                     $rekap1 = SC_Tanah_Rekap_1::find(base64_decode($data['id_sc_rekap']));
                     $rekap2 = SC_Tanah_Rekap_2::find(base64_decode($data['id_sc_rekap']));
 
-                    if ($rekap1 != null && isset($data['data_1_' . $i]) && $data['data_1_' . $i] !== null && empty($data['bangunan_1_' . $i])) {
+                    if ($rekap1 != null && !empty($data['data_1_' . $i]) && empty($data['bangunan_1_' . $i])) {
                         $rekap1->id_muk = $id_muk;
                         $rekap1->id_jaminan_pertanahan = $id_tanah;
                         $rekap1->nilai_njop = $this->normalizeNumber($data['nilai_njop_' . $i]);
